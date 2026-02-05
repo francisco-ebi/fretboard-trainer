6a1cdd340070f37f1c18612ddb6eb867fec4f306
José Francisco Peña <jose.francisco.pees@gmail.com>
2026-02-05 15:02:52 +0000

    feat: added dropout layer to model

--- src/utils/audio/model.ts
+++ src/utils/audio/model.ts
@@ -11,6 +11,7 @@ export async function createModel(): Promise<LayersModel> {
     const tf = await getTiF();
     const model = tf.sequential();
     model.add(tf.layers.dense({ inputShape: [18], units: 32, activation: 'relu' }));
+    model.add(tf.layers.dropout({ rate: 0.2 }));
     model.add(tf.layers.dense({ units: 16, activation: 'relu' }));
     model.add(tf.layers.dense({ units: 6, activation: 'softmax' }));
     model.compile({
